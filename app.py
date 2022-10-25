ea1633017319c5c8c69477b498d0561ca0c2ef68
gabrielcolli <gabrielcolli@microsoft.com>
2022-10-25 11:22:34 -0500

    added block content to base html

--- app.py
+++ app.py
@@ -6,13 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/base")
-def base():
-    return render_template('base.html')
+@app.route("/aboutus")
+def aboutus():
+    return render_template('aboutus.html')
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route("/services")
+def services():
+    return render_template('services.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
