aaa439933c28b58a3f71566eda410810387e49d6
gabrielcolli <gabrielcolli@microsoft.com>
2022-10-26 16:52:56 -0500

    formatted for azure web app

--- sparkon_app/views.py
+++ sparkon_app/views.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, session
-from contact import send_email
-
-app = Flask(__name__)
+from . import app
+import smtplib
+def send_email(name, email, message, subject):
+    yahoopass = "vzksfrmmeajybatk"
+    my_email = "gabrielcolli.sr@yahoo.com"
+    with smtplib.SMTP("smtp.mail.yahoo.com", port = 587) as connection:
+        connection.starttls()
+        connection.login(user = my_email, password = yahoopass)
+        connection.sendmail(from_addr = my_email, to_addrs = "python.sparkon@gmail.com", msg = f"Subject: {subject}\n\n{name}\n{email} has sent you a message:\n\n{message}")
 
 @app.route("/")
 def home():
